c04fac9ad5d478f950e53438f243ad791968bc01
Laurion <klauriondareich@gmail.com>
2019-07-01 13:30:01 +0200

    modification du code javascript

--- file.js
+++ file.js
@@ -1,32 +1,59 @@
-<div class="col-lg-4 col-sm-12" id="reset2">
-  <div class="info-contact">
-    <span>PHONE</span>
-    <a href="tel:+241 04 68 95 25">+241 04 68 95 25</a>
-    <span>EMAIL</span>
-    <a href="mailto:klauriondareich@gmail.com">klauriondareich@gmail.com</a>
-    <a href="mailto:kouhouinikina@gmail.com">kouhouinikina@gmail.com</a>
-    <span>ADRESSE</span>
-    <address>
-      Quartier IAI
-    </address>
-  </div>
-</div>
-<div class="col-lg-8" id="reset">
-  <div class="contact-form">
-    <div class="contact-me">
-      <h2>Contactez Moi</h2>
-    </div>
-    <form action="#" method="GET">
-      <div class="input-content">
-        <input type="text" placeholder="Votre nom">
-        <input type="email" placeholder="Votre email">
-      </div>
-      <div class="input-content">
-        <textarea name="message" id="" cols="30" rows="10">
-          Saisir le message ici ...
-        </textarea>
-      </div>
-      <button>Envoyer</button>
-    </form>
-  </div>
-</div>
+  //Dream and Code
+  // Traitement des informations saisies dans le formulaire
+
+  let userNameFormat = /[a-z]/,
+      userEmailFormat = /.+@.+\..+/,
+      invalidUserName = document.getElementById("invalidusername"),
+      invalidUserEmail = document.getElementById("invalidmail"),
+      userName = document.getElementById("username"),
+      userEmail = document.getElementById("useremail"),
+      form = document.getElementById("formulaire"),
+      validateMessage = document.getElementById("validatemessage"),
+      submitButton = document.getElementById("button");
+
+  // Evenement Blur : vérification du mail de l'utilisateur avant l'envoi du formulaire
+
+  userEmail.addEventListener("blur", function(){
+    userEmail.value === true;
+    if (userEmailFormat.test(userEmail.value) === false) {
+        invalidUserEmail.innerHTML = "Saisissez une adresse email correct";
+        form.preventDefault;
+      }
+    })
+
+  // Evenement Blur : vérification du nom d'utilisateur avant l'envoi du formulaire
+
+    userName.addEventListener("blur", function(){
+      if ((userNameFormat.test(userName.value) === false) || (/[A-Z]/.test((userName.value)[0]) === false)){
+        invalidUserName.innerHTML = "La première lettre doit être en majuscule";
+        form.preventDefault;
+      }
+    })
+
+    // Evenement click: Vérification des informations après l'envoi du formulaire
+
+    submitButton.addEventListener("click", function(){
+      if (username.value === "" || useremail.value ===""){
+        validateMessage.innerHTML = "Veuillez renseigner tous les champs";
+        form.preventDefault;
+      }
+  });
+
+  // Vérification des informations de la newsletter
+
+  let newsletterButton = document.getElementById("subscribe"),
+      newsletterInput = document.getElementById("newsletterinput"),
+      infoNewsletter = document.getElementById("infonewsletter"),
+      newsletterForm = document.getElementById("newsform");
+      newsletterButton.addEventListener("click", function(e){
+    if (userEmailFormat.test(newsletterInput.value) === false){
+      infoNewsletter.innerHTML = "Saisissez une adresse email correct";
+      newsletterForm.preventDefault;
+    }
+    else{
+      newsletterForm.style.display="none";
+      infoNewsletter.style.color="green";
+      infoNewsletter.innerHTML = "votre souscription est réussi";
+    }
+  })
+
